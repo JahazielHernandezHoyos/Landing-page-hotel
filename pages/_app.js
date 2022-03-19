efbe583352ac7d82132cd167c3f1297eec11c20e
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-03-19 17:40:20 -0500

    correccion bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import "../public/css/bootstrap.min.css";
 import "../public/css/index.css";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+
 
 
 
